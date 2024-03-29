3a1e21459c0cff06379ee86dd624591ceaaf4a21
TamNguyen8021 <tamntt8021@gmail.com>
2024-03-29 14:41:02 +0700

    build: change frontend port and update url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
 	production: false,
 	appName: "Udagram",
 	apiHost:
-		"http://af3c6f9b57ed44c3b83574ee339ef8a2-366836917.us-east-1.elb.amazonaws.com:8080/api/v0",
+		"a31071aa9159b4dd68802694ce7a397c-1355970427.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
